32003dac5296ddd17f18bd2a48aa38b68f3855bd
Himanshu Babbar <himanshubabbar1806@gmail.com>
2023-04-12 23:44:35 +0530

    added new functions for routings

--- index.py
+++ index.py
@@ -1,40 +1,18 @@
-# from flask import Flask,render_template
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def index():
-#     return render_template('index.html')
-
-
-
-# # end point-http://127.0.0.1:5000/
-# # context path- /ananya
-
-# @app.route("/about")
-# def ananya():
-#     return render_template('about.html')
-
-# if __name__ == '__main__':
-#     app.run()
-    
-
-# from flask import Flask, render_template
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#    return render_template('index.html')
-# if __name__ == '__main__':
-#    app.run(debug=True)
-
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
    return render_template('index.html')
-   
+
+@app.route('/about')
+def about():
+   return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+   return render_template('index.html')
+
 if __name__ == '__main__':
    app.run(debug=True)
 
