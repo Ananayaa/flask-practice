923120f158d3731dc5fc50dca5228f411500f6c4
ananayaa <ananyaa29k@gmail.com>
2023-04-14 04:55:49 +0530

    login-signup-contact page using jinjja template

--- index.py
+++ index.py
@@ -7,11 +7,15 @@ def home():
 
 @app.route('/about')
 def about():
-   return render_template('index.html')
+   return render_template('about.html')
 
 @app.route('/contact')
 def contact():
-   return render_template('index.html')
+   return render_template('contact.html')
+
+@app.route('/signup')
+def signup():
+   return render_template('signup.html')
 
 if __name__ == '__main__':
    app.run(debug=True)
