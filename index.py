3b84f4f95d69f1e2ff79a2ee1ebaea0dfc6ae924
Himanshu Babbar <himanshubabbar1806@gmail.com>
2023-04-14 13:29:50 +0530

    structured the code & added login page

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request 
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,14 +9,24 @@ def home():
 def about():
    return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact', methods = ["POST","GET"])
 def contact():
+   if request.method == "POST":
+      name = request.form.get("name")
+      email = request.form.get("email")
+      phone = request.form.get("phone")
+      message = request.form.get("message")
+      print(name,email,phone,message)
    return render_template('contact.html')
 
 @app.route('/signup')
 def signup():
    return render_template('signup.html')
 
+@app.route('/login')
+def login():
+   return render_template('login.html')
+
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run(host = "0.0.0.0",debug=True,port="5001")
 
